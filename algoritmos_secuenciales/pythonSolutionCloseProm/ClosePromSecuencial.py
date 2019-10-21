dc778c3327d95d6f2ebfb3b8421f756da8e8ee0c
Santiago <sbaenar1@eafit.edu.co>
2019-10-20 21:26:54 -0500

    Cambios

---
+++ algoritmos_secuenciales/pythonSolutionCloseProm/ClosePromSecuencial.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import time
+
+start_time = time.time()
+datafile = pd.read_csv('../../data/Data2017EmpresaV2.csv', sep=';', index_col='Date', parse_dates=['Date'])
+
+openavrg =  datafile["Open"].mean()
+closeavrg = datafile["Close"].mean()
+
+avrgStockValue = closeavrg - openavrg
+
+print("El valor promedio de las acciones durante todo el tiempo ha sido de: ", avrgStockValue)
+print("--- %s seconds ---" % (time.time() - start_time))
