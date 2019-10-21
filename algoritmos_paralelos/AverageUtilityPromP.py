43386a1d84aeb8e0078911131860d598cc451dd1
Santiago <sbaenar1@eafit.edu.co>
2019-10-21 16:17:34 -0500

    Numba
    Numba casi funcionando, solo resta poner el tiempo de ejecución

--- algoritmos_paralelos/AverageUtilityPromP.py
+++ algoritmos_paralelos/AverageUtilityPromP.py
@@ -1,10 +1,14 @@
+#!/usr/bin/python
+
 import csv
-from numba import njit
+import njit
 
 @njit(parallel=True, fastmath=True)
 def archivo():
     with open('../data/Data2017EmpresaV2.csv') as csvfile:
         readCSV = csv.reader(csvfile, delimiter=';')
+        openSum = 0
+        closeSum = 0
         dates = []
         opens = []
         high = []
@@ -24,6 +28,18 @@ def archivo():
             high.append(hig)
             low.append(lo)
             close.append(clos)
-            volume.append(volum)
+            volume.append(volum)        
+
+        for x in range(len(opens)-1):
+            temp = float(opens[x+1])
+            openSum = openSum + temp
+            openSum = openSum / len(opens)
+
+        for y in range(len(close)-1):
+            temp1 = float(close[y+1])
+            closeSum = closeSum + temp1
+            closeSum = closeSum / len(close)
+
+        print(float(closeSum - openSum))
 
 archivo()
